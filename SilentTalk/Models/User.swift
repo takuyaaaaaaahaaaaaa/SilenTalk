c81c21a450e2f1e6dce4003defc5f947650c2dbc
冨永 拓弥 <t.tominaga@tcmobile.jp>
2021-02-19 03:07:14 +0900

    User型にcustomSizeフィールドを追加

--- SilentTalk/Models/User.swift
+++ SilentTalk/Models/User.swift
@@ -6,8 +6,30 @@
 //
 
 struct User: Hashable, Codable {
+    var customSize: Double = 0
     var headMessage: String
     var bottomMessage: String
     var answerCard: [AnswerCard]
     var memo: String
+    
+    private enum CodingKeys:CodingKey {
+        case customSize
+        case headMessage
+        case bottomMessage
+        case answerCard
+        case memo
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        headMessage = try values.decode(String.self, forKey: .headMessage)
+        bottomMessage = try values.decode(String.self, forKey: .bottomMessage)
+        answerCard = try values.decode([AnswerCard].self, forKey: .answerCard)
+        memo = try values.decode(String.self, forKey: .memo)
+        do {
+            customSize = try values.decode(Double.self, forKey: .customSize)
+        } catch DecodingError.keyNotFound{
+            customSize = 0
+        }
+    }
 }
